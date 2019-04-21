85d9de82907297fe96c902a88884217c56777dd0
Priyesh <49675125+priyesh014@users.noreply.github.com>
2019-04-21 22:16:38 +0530

    Add files via upload

---
+++ Array Operations.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+int n;
+void read(int a[10])
+{
+    int i;
+    printf("Enter elements\n");
+    for(i=0;i<n;i++)
+        scanf("%d",&a[i]);
+}
+void print(int a[10])
+{
+    int i;
+    printf("Elements of array are:\n");
+    for(i=0;i<n;i++)
+        printf("%d",a[i]);
+}
+void insert(int a[10],int ele,int pos)
+{
+    int i;
+    for(i=n-1;i>=pos-1;i--)
+        a[i+1]=a[i];
+    a[pos-1]=ele;
+    n++;
+}
+int delete(int a[10],int pos)
+{
+    int i,ele;
+    ele=a[pos-1];
+    for(i=pos-1;i<n;i++)
+        a[i]=a[i+1];
+    n--;
+    return ele;
+}
+int main()
+{
+    int ch,ele,pos;
+    int a[10];
+    while(1)
+    {
+        printf("\n1.Read\t2.Print\t3.Insert\t4.Delete\t5.Exit\n");
+        scanf("%d",&ch);
+        switch(ch)
+        {
+            case 1:printf("Enter the number of terms\n");
+                    scanf("%d",&n);
+                    read(a);
+                    break;
+            case 2:print(a);
+                    break;
+            case 3:printf("Enter position\n");
+                    scanf("%d",&pos);
+                    printf("Enter element \n");
+                    scanf("%d",&ele);
+                    insert(a,ele,pos);
+                    break;
+            case 4:printf("Enter position\n");
+                    scanf("%d",&pos);
+                    ele=delete(a,pos);
+                    printf("Element deleted is:%d\n",ele);
+                    break;
+            case 5:exit(0);
+        }
+    }
+    return 0;
+}
+
